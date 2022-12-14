e8805dabad940f232910a8718bb5c2e16180f86b
TeachAkk <96035736+s2che@users.noreply.github.com>
2022-12-14 03:36:09 +0300

    Update db.py

---
+++ db.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtSql import QSqlDatabase
+
+def createConnection(databaseName):
+    """Create and open a database connection."""
+    connection = QSqlDatabase.addDatabase("QSQLITE")
+    connection.setDatabaseName(databaseName)
+
+    if not connection.open():
+        QMessageBox.warning(
+            None,
+            "RP Contact",
+            f"Database Error: {connection.lastError().text()}",
+        )
+        return False
+
+    return True
