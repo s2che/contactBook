af6c0cd96489b53c896329b41f43d08d44cc7c04
TeachAkk <96035736+s2che@users.noreply.github.com>
2022-12-14 03:33:30 +0300

    Update vvs.py

--- vvs.py
+++ vvs.py
@@ -1,13 +1,36 @@
-from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QWidget
+from PyQt5.QtWidgets import (
+    QAbstractItemView,
+    QHBoxLayout,
+    QMainWindow,
+    QPushButton,
+    QTableView,
+    QVBoxLayout,
+    QWidget,
+)
 
 class Window(QMainWindow):
-
+    """Main Window."""
     def __init__(self, parent=None):
+        """Initializer."""
+        # Snip...
 
-        super().__init__(parent)
-        self.setWindowTitle("Контакты")
-        self.resize(550, 250)
-        self.centralWidget = QWidget()
-        self.setCentralWidget(self.centralWidget)
-        self.layout = QHBoxLayout()
-        self.centralWidget.setLayout(self.layout)
+        self.setupUI()
+
+    def setupUI(self):
+        """Setup the main window's GUI."""
+        # Create the table view widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+        # Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+        # Lay out the GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
