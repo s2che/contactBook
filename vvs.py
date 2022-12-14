9d2061d26c4e2300baef8b3eb62262b98b58922d
TeachAkk <96035736+s2che@users.noreply.github.com>
2022-12-14 03:28:23 +0300

    Add files via upload

---
+++ vvs.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QWidget
+
+class Window(QMainWindow):
+
+    def __init__(self, parent=None):
+
+        super().__init__(parent)
+        self.setWindowTitle("Контакты")
+        self.resize(550, 250)
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+        self.layout = QHBoxLayout()
+        self.centralWidget.setLayout(self.layout)
